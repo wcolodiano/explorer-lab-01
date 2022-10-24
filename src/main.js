d33c0ff993fa54c2ac423570352f9cb71ad3991a
wcolodiano <washington.wcp@gmail.com>
2022-10-23 23:32:27 -0300

    aula dois finalizada

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import "./css/index.css"
+import Imask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
-
-
-
 
 function setCardType(type) {
     const colors = {
@@ -16,6 +14,61 @@ function setCardType(type) {
     ccBgColor01.setAttribute("fill", colors[type] [0])
     ccBgColor02.setAttribute("fill", colors[type] [1])
     ccLogo.setAttribute("src", `cc-${type}.svg`)
-}
+} 
 
 globalThis.setCardType = setCardType
+
+// security code - alteração do CVC no index.html
+const securityCode = document.querySelector('#security-code')
+const securityCodePattern = {
+    mask: "0000"
+}
+const securityCodeMasked = Imask(securityCode, securityCodePattern)
+
+// Expiração de data - regras para validação de data de validade do cartão expiration-date
+
+const expirationDate = document.querySelector('#expiration-date')
+const expirationDatePattern = {
+    mask: "MM{/}YY",
+    blocks: {
+        MM: {
+            mask: Imask.MaskedRange,
+            from: 1,
+            to: 12,
+        },
+        YY: {
+            mask: Imask.MaskedRange,
+            from: String(new Date().getFullYear()).slice(2), 
+            to: String(new Date().getFullYear() + 10).slice(2),
+        },
+    }
+}
+const expirationDateMasked = Imask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+    mask: [
+        {
+            mask: "0000 0000 0000 0000",
+            regex: /^4\d{0,15}/,
+            cardtype: "visa",
+        },
+        {
+            mask: "0000 0000 0000 0000",
+            regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+            cardtype: "mastercard",
+        },
+        {
+            mask: "0000 0000 0000 0000",
+            cardtype: "default",
+        },
+    ],
+    dispatch: function(appended, dynamicMasked) {
+        const number = (dynamicMasked.value + appended).replace(/\D/g,'')
+        const foundMask = dynamicMasked.compiledMasks.find(function(item) {
+            return number.match(item.regex)
+        })
+        return foundMask
+    }
+}
+const cardNumberMasked = Imask(cardNumber, cardNumberPattern)
